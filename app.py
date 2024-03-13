ba25eb77837a8a16906c4f66444a2f93de2a19ee
YIHSIU18 <yihsiu.lin@ipilyon.net>
2024-03-13 18:44:46 +0100

    3rd commit

--- app.py
+++ app.py
@@ -1,39 +1,67 @@
-from flask import Flask, render_template, redirect, url_for, request, @login_required
+import os
+from flask import Flask, render_template, request, redirect, session, flash
+from datetime import date
+
+#Defining Flask ap
 app = Flask(__name__)
+app.secret_key = "key"
+
+#User credentials
+users = ('admin', 'admin')
+
 #Flask-SQLit
 import sqlite3
 
 #, make_response
 @app.route('/')
 def home():
-    return 'Todo List'
-@app.route('/about')
-def about():
-    return 'about page'
-@app.route('/register')
-def register():
-    return 'register page'
-
+    if 'username' not in session:
+        return redirect('/login')
+    if 'todos' not in session:
+        session['todos'] = {'todo': [], 'In Progress': [], 'complete': []}  # Initialize as a dictionary
+    return render_template('home.html', todos=session['todos'])
 #Login page    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        username = request.form['username']
+        password = request.form['password']
+        # Validation user = admin password = admin
+        if username == 'admin' and password == 'admin':
+            session['username'] = username
+            return redirect('/')
         else:
-            session.['logged_in'] = True
-            flash('You were logged in.')
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
+            flash('Invalid username or password', 'error')
+    return render_template('login.html')
 #Logout page
 @app.route('/logout')
-@login_required
 def logout():
-    session.pop('logged_in',None)
-    flash('You were logged out.')
-    return redirect(url_for('login.html'))
+    session.pop('username',None)
+    flash('You have been logged out.', 'success')
+    return redirect('/login')
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    if 'username' not in session:
+        return redirect('/login')
+    todo = request.form.get('status')
+    if todo and status:
+        session['todos'][status].append(todo)
+        flash('You have added a toto list', 'success')
+    else:
+        flash('Please add a todo list', 'error')
+    return redirect('/')
+
+@app.route('/remove/<int:index>')
+def remove_todo(index):
+    if 'username' not in session:
+        return redirect('/login')
+    if 'todos' in session and 0 <= index < len(session['todos'][status]):
+        del session['todos'][status][index] 
+        flash('Todo removed!','success')
+    else:
+        flash('Error removing todo','error')
+    return redirect("/")
 
 @app.route('/erro')
 def other_page(page_name):
