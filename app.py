464d2b4ee81d21b7adf167f2bd4220d967415ed2
YIHSIU18 <yihsiu.lin@ipilyon.net>
2024-03-14 17:00:02 +0100

    Added database

--- app.py
+++ app.py
@@ -1,34 +1,65 @@
 import os
-from flask import Flask, render_template, request, redirect, session, flash
-from datetime import date
+import sqlite3 
+from flask import Flask, render_template, request, redirect, session, flash, g
+from pathlib import Path
 
 #Defining Flask ap
 app = Flask(__name__)
 app.secret_key = "key"
 
 #User credentials
-users = ('admin', 'admin')
+#users = ('admin', 'admin')
 
 #Flask-SQLit
-import sqlite3
+DATABASE = 'database.db'
 
-#, make_response
+#Connect to database SQLite3
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+#Disconnect from database SQLite
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+#
 @app.route('/')
 def home():
     if 'username' not in session:
         return redirect('/login')
-    if 'todos' not in session:
-        session['todos'] = {'todo': [], 'In Progress': [], 'complete': []}  # Initialize as a dictionary
-    return render_template('home.html', todos=session['todos'])
+    cur = get_db().cursor()
+    #Execute the SQL auery to retrieve tasks from database
+    cur.execute("SELECT * FROM todos")
+    #Fetch all the rows from database
+    tasks = cur.fetchall()
+        # Create a dictionary to store tasks categorized by status
+    todos = {'todo': [], 'In Progress': [], 'complete': []}
+    for task in tasks:
+        # Append each task to the corresponding status list in the dictionary
+        todos[task['status']].append(task)
+    
+    return render_template('home.html', todos=todos)
+
 #Login page    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        cur = get_db().cursor()
+        cur.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password));
+        user = cur.fetchone()
         # Validation user = admin password = admin
-        if username == 'admin' and password == 'admin':
+        if user:
             session['username'] = username
+            flash('Logged in ok', 'success')
             return redirect('/')
         else:
             flash('Invalid username or password', 'error')
@@ -36,7 +67,7 @@ def login():
 #Logout page
 @app.route('/logout')
 def logout():
-    session.pop('username',None)
+    session.clear()
     flash('You have been logged out.', 'success')
     return redirect('/login')
 
@@ -47,11 +78,18 @@ def add_todo():
     todo = request.form.get('todo')
     status = request.form.get('status')
     if todo and status:
-        if status in session['todos']: #Check if the status exists in session['todos']
-            session['todos'][status].append(todo)
-            flash('Todo added successfully!', 'success')
-        else:
-            flash('Invalid status!', 'error')
+        #Get database cursor
+        cur = get_db().cursor()
+        try:
+            #Execute SQL auery to insert todo into the database
+            cur.execute("INSERT INTO todos(taskname, status) VALUES(?,?)", (todo, status))
+            #Commit changes
+            get_db().commit()
+            flash('Todo added okay', 'Success')
+        except Exception as e:
+            # Rollback the transaction if an error occurs
+            get_db().rollback()
+            flash(f'Error: {e}', 'error')
     else:
         flash('Todo or status cannot be empty!', 'error')
     return redirect('/')
@@ -73,5 +111,11 @@ def other_page(page_name):
                             % page_name, 404)
     return response
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#Chekc if the database is NULL then create one
+if not Path('database.db').exists():
+    #Read SQL file from todo.sql
+    sql = Path('todo.sql').read_text()
+    with app.app_context():
+        db = get_db() #connect to db
+        db.cursor().executescript(sql)
+        db.commit()
---
+++ todo.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS todos;
+
+--Table users
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+INSERT INTO users VALUES (1,'admin','admin');
+
+--Table todo list
+CREATE TABLE todos(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  taskname TEXT UNIQUE NOT NULL,
+  status TEXT UNIQUE NOT NULL
+);
